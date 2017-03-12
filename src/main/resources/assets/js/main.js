ab36a950a429051e5708bab9137ef85965a356f0
Lukas <lukas.fredriksson@metria.se>
2017-03-12 02:02:57 +0100

    Första utkast för hållplatser

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -75,5 +75,46 @@ function initGps(map) {
         var coordinates = geolocation.getPosition();
         positionFeature.setGeometry(coordinates ?
           new ol.geom.Point(coordinates) : null);
+
+        var lonlat = ol.proj.transform(coordinates, 'EPSG:3857', 'EPSG:4326');
+
+        var request = "https://api.resrobot.se/v2/location.nearbystops?key=5640c8c2-224d-4225-9040-8047dece61fa&originCoordLat="
+        + lonlat[1] + "&originCoordLong="
+        + lonlat[0] + "&format=json";
+
+        $.ajax({
+            url: request, dataType: 'json', success: function (response) {
+                var points = [];
+                response.StopLocation.forEach(function (item) {
+                    var stop = new ol.Feature({
+                        geometry: new ol.geom.Point(ol.proj.transform([item.lon, item.lat], 'EPSG:4326',
+                          'EPSG:3857')),
+                        name: item.name
+                    });
+                    points.push(stop);
+                });
+                console.log(points);
+
+                var iconStyle = new ol.style.Style({
+                    image: new ol.style.Icon(({
+                        anchor: [0.5, 46],
+                        anchorXUnits: 'fraction',
+                        anchorYUnits: 'pixels',
+                        opacity: 0.75,
+                        src: 'usefravatar.png'
+                    }))
+                });
+
+                var vectorSource = new ol.source.Vector({
+                    features: points //add an array of features
+                });
+
+                var vectorLayer = new ol.layer.Vector({
+                    map: map,
+                    source: vectorSource,
+                    style: iconStyle
+                });
+            }
+        });
     });
 }
